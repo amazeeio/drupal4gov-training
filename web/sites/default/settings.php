367839d6c8dad63f71559e13ec6b217f4146134f
Michael Schmid <michael@amazee.com>
2019-12-03 09:59:37 -0500

    update drupal, add admin_toolbar, install umami

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -38,76 +38,76 @@ if(getenv('LAGOON')){
   );
 }
 
-### Lagoon Solr connection
-// WARNING: you have to create a search_api server having "solr" machine name at
-// /admin/config/search/search-api/add-server to make this work.
-if (getenv('LAGOON')) {
-  $config['search_api.server.solr']['backend_config']['connector_config']['host'] = getenv('SOLR_HOST') ?: 'solr';
-  $config['search_api.server.solr']['backend_config']['connector_config']['path'] = '/solr/';
-  $config['search_api.server.solr']['backend_config']['connector_config']['core'] = getenv('SOLR_CORE') ?: 'drupal';
-  $config['search_api.server.solr']['backend_config']['connector_config']['port'] = 8983;
-  $config['search_api.server.solr']['backend_config']['connector_config']['http_user'] = (getenv('SOLR_USER') ?: '');
-  $config['search_api.server.solr']['backend_config']['connector_config']['http']['http_user'] = (getenv('SOLR_USER') ?: '');
-  $config['search_api.server.solr']['backend_config']['connector_config']['http_pass'] = (getenv('SOLR_PASSWORD') ?: '');
-  $config['search_api.server.solr']['backend_config']['connector_config']['http']['http_pass'] = (getenv('SOLR_PASSWORD') ?: '');
-  $config['search_api.server.solr']['name'] = 'Lagoon Solr - Environment: ' . getenv('LAGOON_PROJECT');
-}
+// ### Lagoon Solr connection
+// // WARNING: you have to create a search_api server having "solr" machine name at
+// // /admin/config/search/search-api/add-server to make this work.
+// if (getenv('LAGOON')) {
+//   $config['search_api.server.solr']['backend_config']['connector_config']['host'] = getenv('SOLR_HOST') ?: 'solr';
+//   $config['search_api.server.solr']['backend_config']['connector_config']['path'] = '/solr/';
+//   $config['search_api.server.solr']['backend_config']['connector_config']['core'] = getenv('SOLR_CORE') ?: 'drupal';
+//   $config['search_api.server.solr']['backend_config']['connector_config']['port'] = 8983;
+//   $config['search_api.server.solr']['backend_config']['connector_config']['http_user'] = (getenv('SOLR_USER') ?: '');
+//   $config['search_api.server.solr']['backend_config']['connector_config']['http']['http_user'] = (getenv('SOLR_USER') ?: '');
+//   $config['search_api.server.solr']['backend_config']['connector_config']['http_pass'] = (getenv('SOLR_PASSWORD') ?: '');
+//   $config['search_api.server.solr']['backend_config']['connector_config']['http']['http_pass'] = (getenv('SOLR_PASSWORD') ?: '');
+//   $config['search_api.server.solr']['name'] = 'Lagoon Solr - Environment: ' . getenv('LAGOON_PROJECT');
+// }
 
-### Lagoon Redis connection
-if (getenv('LAGOON')){
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  $settings['redis.connection']['host'] = getenv('REDIS_HOST') ?: 'redis';
-  $settings['redis.connection']['port'] = 6379;
+// ### Lagoon Redis connection
+// if (getenv('LAGOON')){
+//   $settings['redis.connection']['interface'] = 'PhpRedis';
+//   $settings['redis.connection']['host'] = getenv('REDIS_HOST') ?: 'redis';
+//   $settings['redis.connection']['port'] = 6379;
 
-  $settings['cache_prefix']['default'] = getenv('LAGOON_PROJECT') . '_' . getenv('LAGOON_GIT_SAFE_BRANCH');
+//   $settings['cache_prefix']['default'] = getenv('LAGOON_PROJECT') . '_' . getenv('LAGOON_GIT_SAFE_BRANCH');
 
-  # Do not set the cache during installations of Drupal
-  if (!drupal_installation_attempted() && extension_loaded('redis')) {
-    $settings['cache']['default'] = 'cache.backend.redis';
+//   # Do not set the cache during installations of Drupal
+//   if (!drupal_installation_attempted() && extension_loaded('redis')) {
+//     $settings['cache']['default'] = 'cache.backend.redis';
 
-    // Include the default example.services.yml from the module, which will
-    // replace all supported backend services (that currently includes the cache tags
-    // checksum service and the lock backends, check the file for the current list)
-    $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
+//     // Include the default example.services.yml from the module, which will
+//     // replace all supported backend services (that currently includes the cache tags
+//     // checksum service and the lock backends, check the file for the current list)
+//     $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 
-    // Allow the services to work before the Redis module itself is enabled.
-    $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
+//     // Allow the services to work before the Redis module itself is enabled.
+//     $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
 
-    // Manually add the classloader path, this is required for the container cache bin definition below
-    // and allows to use it without the redis module being enabled.
-    $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
+//     // Manually add the classloader path, this is required for the container cache bin definition below
+//     // and allows to use it without the redis module being enabled.
+//     $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
 
-    // Use redis for container cache.
-    // The container cache is used to load the container definition itself, and
-    // thus any configuration stored in the container itself is not available
-    // yet. These lines force the container cache to use Redis rather than the
-    // default SQL cache.
-    $settings['bootstrap_container_definition'] = [
-      'parameters' => [],
-      'services' => [
-        'redis.factory' => [
-          'class' => 'Drupal\redis\ClientFactory',
-        ],
-        'cache.backend.redis' => [
-          'class' => 'Drupal\redis\Cache\CacheBackendFactory',
-          'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
-        ],
-        'cache.container' => [
-          'class' => '\Drupal\redis\Cache\PhpRedis',
-          'factory' => ['@cache.backend.redis', 'get'],
-          'arguments' => ['container'],
-        ],
-        'cache_tags_provider.container' => [
-          'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
-          'arguments' => ['@redis.factory'],
-        ],
-        'serialization.phpserialize' => [
-          'class' => 'Drupal\Component\Serialization\PhpSerialize',
-        ],
-      ],
-    ];
-  }
-}
+//     // Use redis for container cache.
+//     // The container cache is used to load the container definition itself, and
+//     // thus any configuration stored in the container itself is not available
+//     // yet. These lines force the container cache to use Redis rather than the
+//     // default SQL cache.
+//     $settings['bootstrap_container_definition'] = [
+//       'parameters' => [],
+//       'services' => [
+//         'redis.factory' => [
+//           'class' => 'Drupal\redis\ClientFactory',
+//         ],
+//         'cache.backend.redis' => [
+//           'class' => 'Drupal\redis\Cache\CacheBackendFactory',
+//           'arguments' => ['@redis.factory', '@cache_tags_provider.container', '@serialization.phpserialize'],
+//         ],
+//         'cache.container' => [
+//           'class' => '\Drupal\redis\Cache\PhpRedis',
+//           'factory' => ['@cache.backend.redis', 'get'],
+//           'arguments' => ['container'],
+//         ],
+//         'cache_tags_provider.container' => [
+//           'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
+//           'arguments' => ['@redis.factory'],
+//         ],
+//         'serialization.phpserialize' => [
+//           'class' => 'Drupal\Component\Serialization\PhpSerialize',
+//         ],
+//       ],
+//     ];
+//   }
+// }
 
 ### Lagoon Reverse proxy settings
 if (getenv('LAGOON')) {
